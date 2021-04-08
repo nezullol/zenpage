67f778b1cebdee373cc3c13fd8c943afe4db34f0
Chris Scott <mchmav@gmail.com>
2021-04-08 12:55:55 -0500

    Fixed search and annoying bar move on load

--- js/search.js
+++ js/search.js
@@ -1,16 +1,16 @@
-var SEARCH_BASE = 'https://encrypted.google.com/#q=';
+var SEARCH_BASE = 'https://www.google.com/search?q=';
 var searchInput = document.querySelector('.search__input');
 
 searchInput.addEventListener('keydown', handleKeyDown);
 
 function handleKeyDown(event) {
-	var keycode = event.which || event.keyCode;
-	if (keycode === 13) {
-		google();
-	}
+    var keycode = event.which || event.keyCode;
+    if (keycode === 13) {
+        google();
+    }
 }
 
 function google() {
-	var query = searchInput.value;
-	window.location.href = SEARCH_BASE + query;
+    var query = searchInput.value;
+    window.location.href = SEARCH_BASE + query;
 }
